62d81888a0380bde6e15f504bb78bdbdc597a6d1
Александр ЗУев <undress.my.future@gmail.com>
2019-12-05 14:28:04 +0300

    Добавьте файлы проекта.

---
+++ OpenSheets/App.cs
@@ -0,0 +1,62 @@
+﻿#region License
+//Данный код опубликован под лицензией Creative Commons Attribution-NonCommercial-ShareAlike.
+//Разрешено редактировать, изменять и брать данный код за основу для производных в некоммерческих целях,
+//при условии указания авторства и если производные лицензируются на тех же условиях.
+//Программа поставляется "как есть". Автор не несет ответственности за возможные последствия её использования.
+//Зуев Александр, 2019, все права защищены.
+//This code is listed under the Creative Commons Attribution-NonCommercial-ShareAlike license.
+//You may redistribute, remix, tweak, and build upon this work non-commercially, 
+//as long as you credit the author by linking back and license your new creations under the same terms.
+//This code is provided 'as is'. Author disclaims any implied warranty. 
+//Zuev Aleksandr, 2019, all rigths reserved.
+#endregion
+
+#region Usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace OpenSheets
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class App : IExternalApplication
+    {
+        public static string assemblyPath = "";
+        public Result OnStartup(UIControlledApplication application)
+        {
+            assemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            string tabName = "Weandrevit";
+            try { application.CreateRibbonTab(tabName); } catch { }
+            string panelName = "Виды";
+            RibbonPanel panel = null;
+            List<RibbonPanel> tryPanels = application.GetRibbonPanels(tabName).Where(i => i.Name == panelName).ToList();
+            if (tryPanels.Count == 0)
+            {
+                panel = application.CreateRibbonPanel(tabName, panelName);
+            }
+            else
+            {
+                panel = tryPanels.First();
+            }
+
+            PushButton btnBPrint = panel.AddItem(new PushButtonData(
+                "OpenSheets",
+                "Открыть листы",
+                assemblyPath,
+                "OpenSheets.Command")
+                ) as PushButton;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ OpenSheets/Command.cs
@@ -0,0 +1,53 @@
+﻿#region License
+//Данный код опубликован под лицензией Creative Commons Attribution-NonCommercial-ShareAlike.
+//Разрешено редактировать, изменять и брать данный код за основу для производных в некоммерческих целях,
+//при условии указания авторства и если производные лицензируются на тех же условиях.
+//Программа поставляется "как есть". Автор не несет ответственности за возможные последствия её использования.
+//Зуев Александр, 2019, все права защищены.
+//This code is listed under the Creative Commons Attribution-NonCommercial-ShareAlike license.
+//You may redistribute, remix, tweak, and build upon this work non-commercially, 
+//as long as you credit the author by linking back and license your new creations under the same terms.
+//This code is provided 'as is'. Author disclaims any implied warranty. 
+//Zuev Aleksandr, 2019, all rigths reserved.
+#endregion
+
+#region Usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
+
+namespace OpenSheets
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            Selection sel = commandData.Application.ActiveUIDocument.Selection;
+            if(sel.GetElementIds().Count< 2)
+            {
+                message = "Выберите листы в Диспетчере проекта перед запуском";
+                return Result.Failed;
+            }
+
+            List<ViewSheet> sheets = new List<ViewSheet>();
+            foreach(ElementId id in sel.GetElementIds())
+            {
+                ViewSheet vs = doc.GetElement(id) as ViewSheet;
+                if (vs == null) continue;
+
+                commandData.Application.ActiveUIDocument.ActiveView = vs;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
